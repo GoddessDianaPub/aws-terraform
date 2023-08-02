0d8354abab96eb35f879147ec9312b4a9282a48c
DianaCohen <ac171@walla.com>
2023-08-02 20:59:18 +0300

    changed login to eks

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -99,7 +99,7 @@ resource "aws_instance" "jenkins_server" {
   vpc_security_group_ids   = [var.bastion_ssh_from_security_group_id, aws_security_group.jenkins_sg.id]
   iam_instance_profile     = aws_iam_instance_profile.jenkins_server_ec2_profile.name
   
-  user_data                = "${file("${local.module_path}/scripts/jenkins-server-user-data.sh")}"
+#  user_data                = "${file("${local.module_path}/scripts/jenkins-server-user-data.sh")}"
 
   root_block_device {
     volume_type = var.ebs_type_jenkins_server
